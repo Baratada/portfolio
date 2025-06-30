0ae7910c96be01aaeadde9d2dea8a13eccfb3786
baratada <markogames0000@gmail.com>
2025-06-30 14:17:21 +0200

    Changed site design

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Home from "./pages/Home";
 import Scripts from "./pages/Scripts";
 import Projects from "./pages/Projects";
 import Animations from "./pages/Animations";
+import About from "./pages/About";
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -70,6 +71,19 @@ function AnimatedRoutes() {
             </motion.div>
           }
         />
+        <Route
+          path="/about"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4 }}
+            >
+              <About />
+            </motion.div>
+          }
+        />
       </Routes>
     </AnimatePresence>
   );
@@ -78,41 +92,42 @@ function AnimatedRoutes() {
 function App() {
   return (
     <Router>
-      <div
-        style={{
-          backgroundImage: `url(${process.env.PUBLIC_URL}/media/Background.png)`,
-        }}
-        className="App w-screen min-h-screen bg-cover bg-fixed m-0 p-0"
-      >
+      <div className="App w-screen min-h-screen bg-cover bg-fixed m-0 p-0 pb-24">
         {/* Navigation */}
-        <nav className="flex gap-4 justify-center py-4 bg-white/20 shadow-md">
+        <nav className="flex gap-4 pr-5 justify-end py-4 bg-white/20 shadow-md">
           <Link
             to="/"
-            className="text-blue-500 font-bold hover:underline transition hover:text-blue-300"
+            className="text-white font-bold transition hover:text-purple-300 active:text-purple-300"
           >
             Home
           </Link>
           <Link
             to="/scripts"
-            className="text-blue-500 font-bold hover:underline transition hover:text-blue-300"
+            className="text-white font-bold transition hover:text-purple-300 active:text-purple-300"
           >
             Scripts
           </Link>
           <Link
             to="/animations"
-            className="text-blue-500 font-bold hover:underline transition hover:text-blue-300"
+            className="text-white font-bold transition hover:text-purple-300 active:text-purple-300"
           >
             Animations
           </Link>
           <Link
             to="/projects"
-            className="text-blue-500 font-bold hover:underline transition hover:text-blue-300"
+            className="text-white font-bold transition hover:text-purple-300 active:text-purple-300"
           >
             Projects
           </Link>
+             <Link
+            to="/about"
+            className="text-white font-bold transition hover:text-purple-300 active:text-purple-300"
+          >
+            About
+          </Link>
           <a
             href="https://github.com/baratada/Portfolio"
-            className="text-blue-500 font-bold hover:underline transition hover:text-blue-300"
+            className="text-white font-bold transition hover:text-purple-300 active:text-purple-300"
           >
             Source
           </a>
@@ -121,8 +136,6 @@ function App() {
         {/* Routes with animation */}
         <AnimatedRoutes />
 
-        {/* Footer */}
-        <footer className="fixed bottom-0 w-full py-6 text-center bg-white/20 shadow-md text-sm font-semibold"></footer>
       </div>
     </Router>
   );
